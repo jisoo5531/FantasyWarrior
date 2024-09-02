1b04a5004977410b163672a1caf6a541a81f491c
jisoo5531 <jisoo5531@naver.com>
2024-09-03 00:03:08 +0900

    0903

--- Assets/_MyProject/2. Scripts/GamePlay/Player/PlayerSkill.cs
+++ Assets/_MyProject/2. Scripts/GamePlay/Player/PlayerSkill.cs
@@ -49,8 +49,8 @@ public class PlayerSkill : MonoBehaviour
     {
         equipSkills = new List<int>
         {
-            //0, 1, 2, 3
-            4,5,6,7
+            0, 1, 2, 3
+            //4,5,6,8
             //8
         };
     }
