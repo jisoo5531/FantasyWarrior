2649126ff39757b0224e1bdc04fcfd015566eefe
jisoo5531 <62212959+jisoo5531@users.noreply.github.com>
2024-09-05 17:46:21 +0900

    0905

--- Assets/_MyProject/2. Scripts/UI/Player/Skill/UI_SkillEntry.cs
+++ Assets/_MyProject/2. Scripts/UI/Player/Skill/UI_SkillEntry.cs
@@ -13,35 +13,52 @@ public class UI_SkillEntry : MonoBehaviour
     public Image LockImage;
 
     public SkillData skillData;
-    
+
     [HideInInspector] public string tableName;
 
     private GameObject keySetPanel;
 
     private void Awake()
     {
-        equipButton.onClick.AddListener(() => 
-            { 
+        equipButton.onClick.AddListener(() =>
+            {
                 keySetPanel.gameObject.SetActive(true);
                 keySetPanel.GetComponent<UI_SkillKeySetting>().Initialize(skillData.Skill_Order);
             });
+
+        
+    }
+    private void Start()
+    {
+        EventHandler.playerEvent.RegisterPlayerLevelUp(OnLevelUp_UnlockSkill);
+    }
+    private void OnDisable()
+    {
+        EventHandler.playerEvent.UnRegisterPlayerLevelUp(OnLevelUp_UnlockSkill);
     }
     public void Initialize(SkillData skillData, GameObject keySetPanel)
     {
-        UserData userData = DatabaseManager.Instance.userData;      
-        
-        bool isLock = false == SkillManager.Instance.userAvailableSkillList.Exists((x) => { return x.Skill_ID.Equals(skillData.Skill_ID); });
-
-        if (isLock)
-        {
-            LockImage.gameObject.SetActive(true);
-        }
-
+        UserData userData = DatabaseManager.Instance.userData;        
         this.skillData = skillData;
         this.keySetPanel = keySetPanel;
+
+        OnLevelUp_UnlockSkill();
+
         string folderName = $"{userData.CharClass.ToString()}_Skills";
-        Debug.Log(folderName);
-        skillIcon.sprite = Resources.Load<Sprite>($"{folderName}/{skillData.Icon_Name}");        
+        skillIcon.sprite = Resources.Load<Sprite>($"{folderName}/{skillData.Icon_Name}");
         skillName.text = skillData.Skill_Name;
+
+        
+    }
+
+    private void OnLevelUp_UnlockSkill()
+    {
+        Debug.Log(SkillManager.Instance.userAvailableSkillList == null);
+        if (SkillManager.Instance.userAvailableSkillList != null)
+        {
+            bool isLock = false == SkillManager.Instance.userAvailableSkillList.Exists((x) => { return x.Skill_ID.Equals(skillData.Skill_ID); });
+
+            LockImage.gameObject.SetActive(isLock);
+        }        
     }
 }
