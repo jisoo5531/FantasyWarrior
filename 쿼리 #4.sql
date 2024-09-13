b877db83b185b47ed5524901b021c194f7f806bc
jisoo5531 <62212959+jisoo5531@users.noreply.github.com>
2024-09-13 21:33:49 +0900

    Update 쿼리 #4.sql

--- 쿼리 #4.sql
+++ 쿼리 #4.sql
@@ -23,6 +23,5 @@
 #JOIN skills ON userstats.`Level` >= skills.Unlock_Level
 #WHERE userstats.user_id = 1 AND skills.Class = 1;
 
-UPDATE inventory
-SET inventory.Quantity={}
-WHERE inventory.User_ID={} AND inventory.Item_ID={}
+SELECT *
+FROM userskills;
