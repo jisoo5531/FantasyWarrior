83eb8f7c540f42af5642b928eaf4e1a068e83a18
jisoo5531 <62212959+jisoo5531@users.noreply.github.com>
2024-09-25 18:29:13 +0900

    0925

--- Assets/_MyProject/2. Scripts/UI/Shop/UI_ShopItemPrefab.cs
+++ Assets/_MyProject/2. Scripts/UI/Shop/UI_ShopItemPrefab.cs
@@ -8,10 +8,24 @@ public class UI_ShopItemPrefab : MonoBehaviour
 {
     public Image itemIcon;
     public TMP_Text itemNameText;
+    [Header("아이템 상세정보 열람 버튼")]
+    public Button ItemInfoButton;
 
+    private void Awake()
+    {
+        Debug.Log("되지?");
+        ItemInfoButton.onClick.AddListener(OnClickItemInfoButton);
+    }
     public void Initialize(ItemData item)
     {
         itemIcon.sprite = Resources.Load<Sprite>($"Items/Icon/{item.Item_Name}");
         itemNameText.text = item.Item_Name;
     }
+    /// <summary>
+    /// 이 아이템(오브젝트)를 클릭했을 때 상세 정보가 나오게 하기
+    /// </summary>
+    private void OnClickItemInfoButton()
+    {
+        Debug.Log("상점 아이템 상세정보 나오자.");
+    }    
 }
